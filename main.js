2a312a4fdf91149161f5b498331329eadd472736
unknown <vghoghari82@gmail.com>
2020-11-12 12:53:22 +0530

    moved presence data to config

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const Client = require('./client.js');
-const { token, owner } = require('./config.json');
+const { token, owner, clientPresence } = require('./config.json');
 const client = new Client(token, owner);
 
 client.on('ready', () => {
-    client.user.setPresence({ activity:{ name: 'myself coded', type: 'WATCHING'}, status: 'dnd' });
+    client.user.setPresence(clientPresence);
     client.loadCommands();
     client.autoLeaveVoice();
     console.log(`Logged in as ${client.user.tag}`);
