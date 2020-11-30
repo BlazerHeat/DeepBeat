07188260db20b49a9656e63c2eff1c219e076aed
unknown <vghoghari82@gmail.com>
2020-11-30 17:24:19 +0530

    msToTime function updated

--- utils/msToTime.js
+++ utils/msToTime.js
@@ -1,17 +1,19 @@
 module.exports = (duration) => {
         let totalSeconds = (duration / 1000);
-        let days = Math.floor(totalSeconds / 86400);
-        let hours = Math.floor(totalSeconds / 3600);
-        totalSeconds %= 3600;
+
+        let days = Math.floor(totalSeconds / (24 * 60 * 60));
+        console.log(totalSeconds);
+        totalSeconds -= days * 24 * 60 * 60;
+
+        let hours = Math.floor(totalSeconds / (60 * 60));
+        totalSeconds -= hours * 60 * 60;
+
         let minutes = Math.floor(totalSeconds / 60);
-        let seconds = totalSeconds % 60;
+        totalSeconds -= minutes * 60 * 60;
 
+        let seconds = totalSeconds;
 
-        if(days == 0 && hours == 0) return `${minutes}m : ${seconds.toFixed(0)}s`;
+        if (days == 0 && hours == 0) return `${minutes}m : ${seconds.toFixed(0)}s`;
         else if (days == 0 && hours != 0) return `${hours}h : ${minutes}m : ${seconds.toFixed(0)}s`;
         else return `${days}d : ${hours}h : ${minutes}m : ${seconds.toFixed(0)}s`;
-
-        // if (hours != 0 && days == 0) return `${hours}h : ${minutes}m : ${seconds.toFixed(0)}s`;
-        // else if (days != 0) return `${days}d : ${hours}h : ${minutes}m : ${seconds.toFixed(0)}s`;
-        // else return `${minutes}m : ${seconds.toFixed(0)}s`;
 }
