61e06c7677af6026447179d14f7f8d3fa133760f
BlazerHeat <vghoghari82@gmail.com>
2020-12-24 11:56:15 +0530

    added database ping

--- commands/general/ping.js
+++ commands/general/ping.js
@@ -1,6 +1,7 @@
 const Command = require('../../modules/command');
-const embeds = require('../../utils/embeds.js');
-const msToTime = require('../../utils/msToTime.js');
+const embeds = require('../../utils/embeds');
+const msToTime = require('../../utils/msToTime');
+const Users = require('../../database/models/guild');
 
 class PingCommand extends Command {
     constructor(client){
@@ -13,14 +14,23 @@ class PingCommand extends Command {
         });
     }
 
-    run (client, message, args, prefix){
-        return message.channel.send('<a:pinging:715142364074737736> Pinging...').then(msg => {
+    run (client, message, args, prefix){ 
+
+
+        return message.channel.send('<a:pinging:715142364074737736> Pinging...').then(async msg => {
             msg.delete();
             const ping = (msg.createdTimestamp - message.createdTimestamp).toFixed(0);
             const wsping = client.ws.ping;
             const uptime = msToTime(client.uptime);
 
-            return message.channel.send(embeds.loveEmbed(`:heartbeat: \`${wsping}ms\` :hourglass: \`${ping}ms\` :stopwatch: \`${uptime}\``));
+            const then = Date.now();
+            await Users.findOneAndUpdate({ id: message.guild.id }, { id: message.guild.id }, { upsert: true });
+            const now = Date.now();
+
+            const dbPing = (now - then).toFixed(0);
+
+
+            return message.channel.send(embeds.loveEmbed(`:heartbeat: \`${wsping}ms\` :hourglass: \`${ping}ms\` :stopwatch: \`${uptime}\` <:mongo_database:791551907009265665> \`${dbPing}ms\``));
         });
     }
 }
