4a41445466f63aad8f8830dd23273c5bf42fa967
BlazerHeat <vghoghari82@gmail.com>
2020-12-31 09:58:44 +0530

    added cpu stats in stats command

--- commands/general/stats.js
+++ commands/general/stats.js
@@ -1,6 +1,7 @@
 const Command = require('../../modules/command');
 const { MessageEmbed } = require('discord.js');
-const os = require('os')
+const os = require('os');
+const osUtils = require('os-utils')
 
 class InviteCommand extends Command {
     constructor(client) {
@@ -13,26 +14,28 @@ class InviteCommand extends Command {
         });
     }
 
-    run(client, message, args, prefix) {
+    async run(client, message, args, prefix) {
 
         const memoryUsage = os.totalmem() - os.freemem();
-        const memoryUsagePercent = ((memoryUsage/os.totalmem())*100).toFixed(2);
+        const memoryUsagePercent = ((memoryUsage / os.totalmem()) * 100).toFixed(2);
         const cpu = os.cpus()[0].model.split(' ').slice(0, 3).join(' ');
         const platform = os.platform().replace('win32', 'Windows');
         const Thresold = 90;
 
-        const stats = new MessageEmbed()
-            .setAuthor(`${client.user.tag} [${client.user.id}]`, client.user.displayAvatarURL())
-            .setDescription(`**${memoryUsagePercent}% Memory usage \`${(memoryUsage/(1024*1024)).toFixed(0)} MB\`, ${memoryUsagePercent >= Thresold ? "UnHealthy" : "Healthy"}! <a:health:693043046677348474>**`)
-            .addField(`Guilds Streaming :shield: `, `\`${this.client.guilds.cache.size}\``, true)
-            .addField(`Users Serving :busts_in_silhouette: `, `\`${this.client.users.cache.size}\``, true)
-            .addField(`Channels Scanning :file_folder: `, `\`${this.client.channels.cache.filter(x=>x.type==='text').size}\``, true)
-            .addField('CPU <:CPU:793742666759929876>', `\`${cpu}\``, true)
-            .addField('TOTAL MEM <:RAM:793742685731028992>', `\`${(os.totalmem()/(1024*1024*1024)).toFixed(0)} GB\``, true)
-            .addField('PLATFORM <:platform:793743280063774720>', `\`${platform}\``, true);
-            memoryUsagePercent >= Thresold ? stats.setColor(0xFF0000) : stats.setColor(0x00FF78);
-        return message.channel.send(stats);
-
+        osUtils.cpuUsage(cpuUsage => {
+            cpuUsage = (cpuUsage*100).toFixed(2);
+            const stats = new MessageEmbed()
+                .setAuthor(`${client.user.tag} [${client.user.id}]`, client.user.displayAvatarURL())
+                .setDescription(`**System ${memoryUsagePercent >= Thresold || cpuUsage >= Thresold ? "UnHealthy" : "Healthy"}! <a:health:693043046677348474>**\n${cpuUsage}% CPU usage\n${memoryUsagePercent}% Memory usage \`${(memoryUsage / (1024 * 1024)).toFixed(0)} MB\``)
+                .addField(`Guilds Streaming :shield: `, `\`${this.client.guilds.cache.size}\``, true)
+                .addField(`Users Serving :busts_in_silhouette: `, `\`${this.client.users.cache.size}\``, true)
+                .addField(`Channels Scanning :file_folder: `, `\`${this.client.channels.cache.filter(x => x.type === 'text').size}\``, true)
+                .addField('CPU <:CPU:793742666759929876>', `\`${cpu}\``, true)
+                .addField('TOTAL MEM <:RAM:793742685731028992>', `\`${(os.totalmem() / (1024 * 1024 * 1024)).toFixed(0)} GB\``, true)
+                .addField('PLATFORM <:platform:793743280063774720>', `\`${platform}\``, true);
+            memoryUsagePercent >= Thresold || cpuUsage >= Thresold ? stats.setColor(0xFF0000) : stats.setColor(0x00FF78);
+            return message.channel.send(stats);
+        });
     }
 }
 
