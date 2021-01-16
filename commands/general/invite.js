1fbdc7c9e47bf4a644fe5841b4ebb405af557250
BlazerHeat <vghoghari82@gmail.com>
2021-01-16 11:02:40 +0530

    Added Discord Bots List link

--- commands/general/invite.js
+++ commands/general/invite.js
@@ -14,10 +14,12 @@ class InviteCommand extends Command {
 
     run(client, message, args, prefix) {
         const invite = new MessageEmbed()
-            .setAuthor('All Links!', client.user.displayAvatarURL())
-            .setDescription(`[Invite me](https://discord.com/api/oauth2/authorize?client_id=761475247680520193&permissions=3492096&scope=bot)\n[Support Server](https://discord.gg/gTkmen2)\n[Github Repo](https://github.com/BlazerHeat/DeepBeat)\nDiscord Bot List - comming soon\nWebsite - comming soon
-            `)
-            .setColor('#FF087F');
+          .setAuthor("All Links!", client.user.displayAvatarURL())
+          .setDescription(
+            `[Invite me](https://discord.com/api/oauth2/authorize?client_id=761475247680520193&permissions=3492096&scope=bot)\n[Support Server](https://discord.gg/gTkmen2)\n[Github Repo](https://github.com/BlazerHeat/DeepBeat)\n[Discord Bot List](https://top.gg/bot/761475247680520193)\nWebsite - comming soon
+            `
+          )
+          .setColor("#FF087F");
         return message.channel.send(invite);
     }
 }
